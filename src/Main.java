3e064045df16382940c3dbdc540dfe1a916d21b4
ArtOxid <bashkin.artem@gmail.com>
2022-10-28 22:36:53 +0600

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int minimumMiles = 20;
-        int miles = service.calculate(price, minimumMiles);
+        int cost = 10_000;
+        int miles = service.calculate(cost);
         System.out.println(miles);
     }
 }
